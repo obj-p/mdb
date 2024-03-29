36d9608712bbb06e58300cb729f1ce098cc3638e
Jason Prasad <jasongprasad@gmail.com>
2024-03-29 00:42:55 -0400

    Add macos 14

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "mdb",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v14)],
     products: [
         .executable(name: "mdb", targets: ["mdb"]),
     ],

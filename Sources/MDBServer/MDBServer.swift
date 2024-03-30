b52de65b2aa46d10337568de02eb6fa8cf2beac1
Jason Prasad <jasongprasad@gmail.com>
2024-03-30 13:01:27 -0400

    Update /api

--- Sources/MDBServer/MDBServer.swift
+++ Sources/MDBServer/MDBServer.swift
@@ -6,12 +6,14 @@ public extension Application {
     static func companionServiceApp() throws -> Application {
         let environment = Environment(
             name: "development",
+            // Only pass the path through to Vapor's environment. This avoids passing mdb's arguments through.
             arguments: [ProcessInfo.processInfo.arguments[0]]
         )
+
         let app = Application(environment)
         let transport = VaporTransport(routesBuilder: app)
         let handler = CompanionServiceImpl()
-        try handler.registerHandlers(on: transport)
+        try handler.registerHandlers(on: transport, serverURL: CompanionServer.Servers.server1())
         return app
     }
 }
